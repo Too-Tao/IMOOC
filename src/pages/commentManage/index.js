ebd46fc3a0badd50f8efc8bebef4bde6efb4cf8c
Too-Tao <599366020@qq.com>
2019-05-25 15:24:41 +0800

    feat: add some modules

---
+++ src/pages/commentManage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class CommentManage extends Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default CommentManage
---
+++ src/pages/dataAnalysis/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class DataAnalysis extends Component {
+  render () {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default DataAnalysis
