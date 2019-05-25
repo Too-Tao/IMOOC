c1571993559d1857884de3f5127c005ef484eaf6
Too-Tao <599366020@qq.com>
2019-05-25 21:07:22 +0800

    styles:writed some styles

--- src/components/GlobalSideMenu/index.js
+++ src/components/GlobalSideMenu/index.js
@@ -1,11 +1,12 @@
 import React, { PureComponent } from 'react'
 import { Layout, Menu, Icon } from 'antd'
+import { menuRoutesData } from 'routes'
+import Link from 'umi/link'
 import styles from './index.less'
 import logo from 'images/login/logo.png'
 
 const { Sider } = Layout
 const MenuItem = Menu.Item
-const SubMenu = Menu.SubMenu
 const getIcon = () => (
   <img className={styles.logoImg} src={logo} alt="logo" />
 )
@@ -16,55 +17,27 @@ export default class SideMenu extends PureComponent {
   }
   render () {
     const { collapsed } = this.props
+    console.log(this.props)
     return (
       <div>
         <Sider className={styles.sideStyle} trigger={null} collapsible collapsed={collapsed}>
           <div className={styles.logo}>
-            <a href="/">
+            <Link to="/">
               <Icon component={getIcon} />
               <h1>慕课推荐系统</h1>
-            </a>
+            </Link>
           </div>
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <MenuItem key="1">
-              <Icon type="home" />
-              <span>课程推荐</span>
-            </MenuItem>
-            <SubMenu
-              key="sub1"
-              title={
-                <span>
-                  <Icon type="video-camera" />
-                  <span>课程管理</span>
-                </span>
-              }
-            >
-              <MenuItem key="2">
-                <span>全文检索</span>
-              </MenuItem>
-              <MenuItem key="3">
-                <span>分类查询</span>
-              </MenuItem>
-              <MenuItem key="4">
-                <span>查询课程</span>
-              </MenuItem>
-            </SubMenu>
-            <MenuItem key="5">
-              <Icon type="area-chart" />
-              <span>数据分析</span>
-            </MenuItem>
-            <MenuItem key="6">
-              <Icon type="message" />
-              <span>评论管理</span>
-            </MenuItem>
-            <MenuItem key="7">
-              <Icon type="read" />
-              <span>分类管理</span>
-            </MenuItem>
-            <MenuItem key="8">
-              <Icon type="user" />
-              <span>用户管理</span>
-            </MenuItem>
+          <Menu theme="dark" mode="inline">
+            {
+              menuRoutesData.map( ({ path, title, iconType }) => (
+                <MenuItem key={path}>
+                  <Link to={path}>
+                    <Icon type={iconType} />
+                    <span>{title}</span>
+                  </Link>
+                </MenuItem>
+              ))
+            }
           </Menu>
         </Sider>
       </div>
