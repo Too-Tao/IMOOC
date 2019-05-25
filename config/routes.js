ee4b8e857758d242bf9ce3c587601e8f61b0b6b7
Too-Tao <599366020@qq.com>
2019-05-25 15:23:03 +0800

    subject: add routes

--- config/routes.js
+++ config/routes.js
@@ -5,7 +5,33 @@ export default [
     routes: [
       {
         path: '/login',
-        component: './login/index'
+        component: './login/index',
+        title: '登陆'
+      },
+      {
+        path: '/dashboard',
+        component: './dashboard/index',
+        title: '课程推荐'
+      }, {
+        path: '/courseManage',
+        title: '课程管理',
+        component: './courseManage/index'
+      }, {
+        path: './dataAnalysis',
+        title: '数据分析',
+        component: './dataAnalysis/index'
+      }, {
+        path: './commentManage',
+        title: '评论管理',
+        component: './commentManage/index'
+      }, {
+        path: './classifiedManage',
+        title: '分类管理',
+        component: './classifiedManage/index'
+      }, {
+        path: './userManage',
+        title: '用户管理',
+        component: './userManage/index'
       }
     ]
   }
