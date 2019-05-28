d7d108c506ebc28b26bf824d828572b474e02bc4
Too-Tao <599366020@qq.com>
2019-05-28 22:07:15 +0800

    fix: imgUrl is invalidable

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -4,23 +4,22 @@ import courseImg1 from 'images/dashboard/carousel001.jpg'
 import courseImg2 from 'images/dashboard/carousel002.jpg'
 import courseImg3 from 'images/dashboard/carousel003.jpg'
 import courseImg4 from 'images/dashboard/carousel004.jpg'
-import springImg from 'images/dashboard/spring.jpg'
 
 import styles from './styles/index.less'
 
 const courseData = [
-  { id: 0, imgUrl: 'http://chuantu.xyz/t6/702/1558956726x1709417261.jpg', title: 'SpringCloud  Finchley三版本(M2+RELEASE+SR2)微服务实战' },
-  { id: 1, imgUrl: 'http://chuantu.xyz/t6/702/1558957107x1954578459.jpg', title: 'Java电商秒杀系统深度优化 从容应对亿级流量挑战' },
-  { id: 2, imgUrl: 'http://chuantu.xyz/t6/702/1558957137x1709417261.jpg', title: 'Vue2.5开发去哪儿网App 从零基础入门到实战项目'},
-  { id: 3, imgUrl: 'http://chuantu.xyz/t6/702/1558957188x1709417261.jpg', title: 'Node.js从零开发Web Server博客项目  前端晋升全栈工程师必备' },
-  { id: 4, imgUrl: 'http://chuantu.xyz/t6/702/1558957241x1709417261.jpg', title: 'PHP小白零基础入门' },
-  { id: 5, imgUrl: 'http://chuantu.xyz/t6/702/1558957252x1954578459.jpg', title: '微信小程序入门与实战 常用组件API开发技巧项目实战' },
-  { id: 6, imgUrl: 'http://chuantu.xyz/t6/702/1558957269x1709417261.jpg', title: 'Spring Boot 2.0深度实践之系列总览' },
-  { id: 7, imgUrl: 'http://chuantu.xyz/t6/702/1558957281x1954578459.jpg', title: 'Google面试官亲授-Java面试新手尊享课' },
-  { id: 8, imgUrl: 'http://chuantu.xyz/t6/702/1558957294x1709417261.jpg', title: 'JavaScript版 数据结构与算法' },
-  { id: 9, imgUrl: 'http://chuantu.xyz/t6/702/1558957307x1709417261.jpg', title: 'Spring Security技术栈开发企业级认证与授权' },
-  { id: 10, imgUrl: 'http://chuantu.xyz/t6/702/1558957322x1954578459.jpg', title: '新一代大数据计算引擎  Flink从入门到实战' },
-  { id: 11, imgUrl: 'http://chuantu.xyz/t6/702/1558957336x1954578459.jpg', title: '2018 AWS技术峰会  自动化运维、微服务及容器技术分论坛' },
+  { id: 0, imgUrl: 'http://chuantu.xyz/t6/702/1559011567x3030586988.jpg', title: 'SpringCloud  Finchley三版本(M2+RELEASE+SR2)微服务实战' },
+  { id: 1, imgUrl: 'http://chuantu.xyz/t6/702/1559011582x3030586988.jpg', title: 'Java电商秒杀系统深度优化 从容应对亿级流量挑战' },
+  { id: 2, imgUrl: 'http://chuantu.xyz/t6/702/1559011600x3030586988.jpg', title: 'Vue2.5开发去哪儿网App 从零基础入门到实战项目'},
+  { id: 3, imgUrl: 'http://chuantu.xyz/t6/702/1559011567x3030586988.jpg', title: 'Node.js从零开发Web Server博客项目  前端晋升全栈工程师必备' },
+  { id: 4, imgUrl: 'http://chuantu.xyz/t6/702/1559011567x3030586988.jpg', title: 'PHP小白零基础入门' },
+  { id: 5, imgUrl: 'http://chuantu.xyz/t6/702/1559011600x3030586988.jpg', title: '微信小程序入门与实战 常用组件API开发技巧项目实战' },
+  { id: 6, imgUrl: 'http://chuantu.xyz/t6/702/1559011582x3030586988.jpg', title: 'Spring Boot 2.0深度实践之系列总览' },
+  { id: 7, imgUrl: 'http://chuantu.xyz/t6/702/1559011567x3030586988.jpg', title: 'Google面试官亲授-Java面试新手尊享课' },
+  { id: 8, imgUrl: 'http://chuantu.xyz/t6/702/1559011600x3030586988.jpg', title: 'JavaScript版 数据结构与算法' },
+  { id: 9, imgUrl: 'http://chuantu.xyz/t6/702/1559011582x3030586988.jpg', title: 'Spring Security技术栈开发企业级认证与授权' },
+  { id: 10, imgUrl: 'http://chuantu.xyz/t6/702/1559011567x3030586988.jpg', title: '新一代大数据计算引擎  Flink从入门到实战' },
+  { id: 11, imgUrl: 'http://chuantu.xyz/t6/702/1559011600x3030586988.jpg', title: '2018 AWS技术峰会  自动化运维、微服务及容器技术分论坛' },
 ]
 
 class Dashboard extends Component {
