b8b866adbff9dcb734081b8369e92b6021c13eb2
Too-Tao <599366020@qq.com>
2019-05-29 20:25:04 +0800

    feat: add dataAnalysis page

--- src/pages/dataAnalysis/index.js
+++ src/pages/dataAnalysis/index.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import { Row, Col } from 'antd'
+import styles from './styles/index.less'
 
 class DataAnalysis extends Component {
   render () {
     return (
-      <div>DataAnalysis</div>
+      <div>
+        <Row gutter={24}>
+          <Col span={12}>
+            <div className={styles.echartsHeaderItem}></div>
+          </Col>
+          <Col span={12}>
+            <div className={styles.echartsHeaderItem}></div>
+          </Col>
+        </Row>
+        <div className={styles.rowWrap}>
+          <Row gutter={12}>
+            <Col span={8}>
+              <div className={styles.echartsFooterItem}></div>
+            </Col>
+            <Col span={8}>
+              <div className={styles.echartsFooterItem}></div>
+            </Col>
+            <Col span={8}>
+              <div className={styles.echartsFooterItem}></div>
+            </Col>
+          </Row>
+        </div>
+      </div>
     )
   }
 }
