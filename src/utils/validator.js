369b0dde5dbb75c4713c75b93a91ce18d920005f
Too-Tao <599366020@qq.com>
2019-05-30 21:48:10 +0800

    feat: add form validator methods

---
+++ src/utils/validator.js
@@ -0,0 +1,18 @@
+export const validatorName = (rule, value = '', callback) => {
+  if (~value.indexOf('  ')) {
+    callback('用户名不能包含空格')
+  } else {
+    callback()
+  }
+}
+
+export const validatorPassword = (rule, value = '', callback) => {
+  const len = value.length
+  if ((len < 6 || len > 16) && len !== 0) {
+    callback('密码长度在8-16个字符')
+  } else if (~value.indexOf(' ')) {
+    callback('密码不能包含空格')
+  } else {
+    callback()
+  }
+}
