13c0d98e11257d6f81d157c9e23338a69ace7e7a
Too-Tao <599366020@qq.com>
2019-06-04 22:05:49 +0800

    feat: 增加邮箱及手机表单校验

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -16,3 +16,21 @@ export const validatorPassword = (rule, value = '', callback) => {
     callback()
   }
 }
+
+export const validatorEmail = (rule, value = '', callback) => {
+  const reg = /^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/
+  if (!reg.test(value)) {
+    callback('邮箱地址不正确')
+  } else {
+    callback()
+  }
+}
+
+export const validatorPhone = (rule, value = '', callback) => {
+  const mobile = /^(?:\+?86)?1(?:3\d{3}|5[^4\D]\d{2}|8\d{3}|7(?:[35678]\d{2}|4(?:0\d|1[0-2]|9\d))|9[189]\d{2}|66\d{2})\d{6}$/
+  if (!mobile.test(value)) {
+    callback('手机号码不正确')
+  } else {
+    callback()
+  }
+}
