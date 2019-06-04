366dff497b6957e1825ceb4a45fb3cff13ee9cee
Too-Tao <599366020@qq.com>
2019-06-04 22:11:07 +0800

    feat: 在注册页面新增邮箱及联系电话输入

--- src/pages/login/components/register/index.js
+++ src/pages/login/components/register/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { Form, Input, Button, message } from 'antd'
+import { Form, Input, Button, message, AutoComplete } from 'antd'
 import { connect } from 'dva'
 import styles from './styles/index.less'
-import { validatorName, validatorPassword } from 'utils/validator.js'
+import { validatorName, validatorPassword, validatorEmail, validatorPhone } from 'utils/validator.js'
+import { EMAIL_ADDRESS } from 'utils/constants.js'
 
 const FormItem = Form.Item
-
+const AutoCompleteOption = AutoComplete.Option
 const formLayout = {
   labelCol: { span: 6 },
   wrapperCol: { span: 14 },
@@ -13,7 +14,9 @@ const formLayout = {
 
 const rulesData = {
   usernameRules: [{ required: true, message: '请输入用户名' }, { validator: validatorName }],
-  passwordRules: [{ required: true, message: '请输入密码' }, { validator: validatorPassword }]
+  passwordRules: [{ required: true, message: '请输入密码' }, { validator: validatorPassword }],
+  emailRules: [{ required: true, message: '请输入邮箱地址' }, { validator: validatorEmail }],
+  phoneRules: [{ required: true, message: '请输入联系电话' }, { validator: validatorPhone }]
 }
 
 const mapStateToProps = ({ loading }) => {
@@ -25,6 +28,10 @@ const mapStateToProps = ({ loading }) => {
 @connect(mapStateToProps)
 class Registration extends Component {
 
+  state = {
+    autoCompleteResult: []
+  }
+
   comparePassword = (rule, value, callback) => {
     const { getFieldValue } = this.props.form
     if (value !== getFieldValue('password_register')) {
@@ -34,10 +41,27 @@ class Registration extends Component {
     }
   }
 
+  handleSelectEmail = (value) => {
+    let autoCompleteResult
+    if (!value) {
+      autoCompleteResult = []
+    } else {
+      autoCompleteResult = EMAIL_ADDRESS.map(item => (
+        `${value}${item}`
+      ))
+    }
+    this.setState(() => {
+      return {
+        autoCompleteResult
+      }
+    })
+  }
+
   handleSubmitRegister = (e) => {
     const { form, dispatch } = this.props
     e.preventDefault()
     form.validateFields((err, values) => {
+      delete values.confirm_password_register
       if (!err) {
         dispatch({ type: 'login/register', payload: values, callback: this.handleRegisterCallback })
       }
@@ -45,18 +69,22 @@ class Registration extends Component {
   }
 
   handleRegisterCallback = () => {
-    this.handleCancelRegister(false)
+    this.handleCancelRegister()
     message.success('注册成功')
   }
 
-  handleCancelRegister = (flag) => {
-    this.props.handleRegisteredModal(flag)
+  handleCancelRegister = () => {
+    this.props.handleRegisteredModal(false)
     this.props.form.resetFields()
   }
 
   render () {
+    const { autoCompleteResult } = this.state
     const { getFieldDecorator } = this.props.form
-    const { usernameRules, passwordRules } = rulesData
+    const { usernameRules, passwordRules, emailRules, phoneRules } = rulesData
+    const emailOptions = autoCompleteResult.map((item) => (
+      <AutoCompleteOption key={item}>{item}</AutoCompleteOption>
+    ))
     return (
       <Form {...formLayout} colon={false} hideRequiredMark={true} onSubmit={this.handleSubmitRegister}>
         <FormItem label="用户名">
@@ -79,12 +107,35 @@ class Registration extends Component {
         </FormItem>
         <FormItem label="确认密码">
           {getFieldDecorator('confirm_password_register', {
-              rules: [{ required: true, message: '请输入密码' }, { validator: this.comparePassword }],
+              rules: [{ required: true, message: '请确认密码' }, { validator: this.comparePassword }],
               validateTrigger: 'onBlur'
             })(
               <Input type="password" />
             )
           }
+        </FormItem>
+        <FormItem label="邮箱">
+          {getFieldDecorator('email', {
+              rules: emailRules,
+              validateTrigger: 'onBlur'
+            })(
+              <AutoComplete
+                dataSource={emailOptions}
+                onChange={this.handleSelectEmail}
+              >
+                <Input />
+              </AutoComplete>
+            )
+
+          }
+        </FormItem>
+        <FormItem label="联系电话">
+        {getFieldDecorator('phone', {
+            rules: phoneRules,
+            validateTrigger: 'onBlur'
+          })(<Input />)
+
+        }
         </FormItem>
         <FormItem className={styles.buttonStyle}>
           <Button type="primary" htmlType="submit">注册</Button>
