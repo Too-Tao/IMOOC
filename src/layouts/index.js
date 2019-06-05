d7b8a916d8585453a1dc2d36b403e337a6bf39e5
Too-Tao <599366020@qq.com>
2019-06-05 23:02:33 +0800

    feat: 权限校验

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Layout } from 'antd'
+import router from 'umi/router'
+import { connect } from 'dva'
 import GlobalHeader from '@/components/GlobalHeader'
 import GlobalSideMenu from '@/components/GlobalSideMenu'
 import GlobalFooter from '@/components/GlobalFooter'
@@ -7,9 +9,28 @@ import styles  from './index.less';
 
 const { Content  } = Layout
 
+const mapStateToProps = ({ global }) => {
+  return {
+    userInfo: global.userInfo
+  }
+}
+
+@connect(mapStateToProps)
 class BasicLayout extends React.Component {
   state = {
     collapsed: false
+  }
+
+  componentDidMount () {
+    this.props.dispatch({ type: 'global/getUserInfo', callback: this.getUserInfoCallback })
+  }
+
+  getUserInfoCallback = () => {
+    const { pathname } = this.props.location
+    const { userInfo } = this.props
+    if (userInfo.utype === 101001 && (pathname === '/courseManage' || pathname === '/commentManage' || pathname === '/classifiedManage' || pathname === '/userManage')) {
+      router.replace('/')
+    }
   }
 
   changeCollapsed = (flag) => {
--- src/models/index.js
+++ src/models/index.js
@@ -4,11 +4,13 @@ import Cookie from 'cookiejs'
 import { message } from 'antd'
 import { IMOOC_LOGIN_TOKEN } from '@/utils/constants'
 
-const { logoutURL } = api
+const { logoutURL, userInfoURL } = api
 
 export default {
-  namespace: 'globalHeader',
-  state: {},
+  namespace: 'global',
+  state: {
+    userInfo: {}
+  },
   effects: {
     *logout (_, { call }) {
       const logoutStatus = yield call(logoutURL)
@@ -17,6 +19,22 @@ export default {
         Cookie.remove(IMOOC_LOGIN_TOKEN)
         router.push('/login')
       }
-    }
+    },
+    *getUserInfo ({ callback }, { call, put }) {
+      const userInfoData = yield call(userInfoURL)
+      yield put({ type: 'saveUserInfo', payload: userInfoData })
+      if (userInfoData) {
+        callback()
+        return
+      }
+    },
+  },
+  reducers: {
+    saveUserInfo (state, { payload }) {
+      return {
+        ...state,
+        ...payload
+      }
+    },
   }
 }
